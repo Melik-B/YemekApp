5aff75e64413cbfe264c1f049a2a6cae2a32f005
Melik-B <melikbaykal06@gmail.com>
2022-06-30 20:50:37 +0300

    Add project files.

---
+++ DataAccess/Entities/Malzeme.cs
@@ -0,0 +1,14 @@
+﻿using AppCore.Records.Bases;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Entities
+{
+    public class Malzeme : RecordBase
+    {
+        [Required]
+        [StringLength(40)]
+        public string Adi { get; set; }
+
+        public List<YemekTarifi> YemekTarifleri { get; set; }
+    }
+}
---
+++ DataAccess/Entities/Rol.cs
@@ -0,0 +1,15 @@
+﻿using AppCore.Records.Bases;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Entities
+{
+    public class Rol : RecordBase
+    {
+        [Required]
+        [StringLength(20)]
+        public string Adi { get; set; }
+
+        public List<Kullanici> Kullanicilar { get; set; }
+
+    }
+}
